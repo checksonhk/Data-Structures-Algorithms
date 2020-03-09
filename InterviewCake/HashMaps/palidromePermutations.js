0007e13f6119f80a2eebd3b9324633997637c21b
checksonhk <jackson.to@gmail.com>
2020-03-09 15:22:22 -0400

    solution palindrome permutations

--- InterviewCake/HashMaps/palidromePermutations.js
+++ InterviewCake/HashMaps/palidromePermutations.js
@@ -2,29 +2,54 @@
 Write an efficient function that checks whether any permutation of an input string is a palindrome.
 */
 
-function palindromePermutations(string) {
+// Overall Time complexity of O(2n) & Space complexity of O(n)
+function hasPalindromePermutation(string) {
+  // time complexity of O(n) space complexity of O(n)
   const seenChars = string.split('').reduce((obj, char) => {
     if (obj[char]) {
       obj[char]++;
     } else {
       obj[char] = 1;
     }
+    return obj;
   }, {});
+
   // if even
   if (string.length % 2) {
+    let numberofOddChars = 0;
+    // time complexity of O(n)
     for (const char of string) {
-      // if any characters are odd
-      if (!(obj[char] % 2)) return false;
-    }
-    return true;
-  } else {
-    const numberofOddChars = 0;
-    for (const char of string) {
-      if (obj[char] % 2) numberofOddChars++;
+      if (seenChars[char] % 2) numberofOddChars++;
     }
     return numberofOddChars > 1 ? false : true;
+  } else {
+    // time complexity of O(n)
+    for (const char of string) {
+      // if any characters are odd
+      if (seenChars[char] % 2) return false;
+    }
+    return true;
   }
 }
 
-console.log(palindromePermutations('civic'));
-console.log(palindromePermutations('cviic'));
+// Better cleaner solution with Set
+function hasPalindromePermutation(theString) {
+  // Track characters we've seen an odd number of times
+  const unpairedCharacters = new Set();
+
+  for (let char of theString) {
+    if (unpairedCharacters.has(char)) {
+      unpairedCharacters.delete(char);
+    } else {
+      unpairedCharacters.add(char);
+    }
+  }
+
+  // The string has a palindrome permutation if it
+  // has one or zero characters without a pair
+  return unpairedCharacters.size <= 1;
+}
+
+console.log(hasPalindromePermutation('civic'));
+console.log(hasPalindromePermutation('cviic'));
+console.log(hasPalindromePermutation('civil'));
---
+++ InterviewCake/HashMaps/wordMap.js
@@ -0,0 +1,3 @@
+/*
+Write code that takes a long string and builds its word cloud data in a map ↴ , where the keys are words and the values are the number of times the words occurred.
+*/
