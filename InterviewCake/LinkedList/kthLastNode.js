db88a6cf572378c10419b5f8e34c88de0804929a
checksonhk <jackson.to@gmail.com>
2020-03-18 14:28:35 -0400

    kthLastNode.js

--- InterviewCake/LinkedList/kthLastNode.js
+++ InterviewCake/LinkedList/kthLastNode.js
@@ -14,38 +14,34 @@ function kthToLastNode(k, head) {
     throw new Error(`Impossible to find less than first to last node: ${k}`);
   }
 
-  // STEP 1: get the length of the list
-  // Start at 1, not 0
-  // else we'd fail to count the head node!
-  let listLength = 1;
-  let currentNode = head;
+  let leftNode = head;
+  let rightNode = head;
 
-  // Traverse the whole list,
-  // counting all the nodes
-  while (currentNode.next) {
-    currentNode = currentNode.next;
-    listLength += 1;
+  // Move rightNode to the kth node
+  for (let i = 1; i < k; i++) {
+    // But along the way, if a rightNode doesn't have a next,
+    // then k is greater than the length of the list and there
+    // can't be a kth-to-last node! we'll raise an error
+    if (!rightNode.next) {
+      throw new Error(`k is larger than the length of the linked list: ${k}`);
+    }
+
+    rightNode = rightNode.next;
   }
 
-  // If k is greater than the length of the list, there can't
-  // be a kth-to-last node, so we'll return an error!
-  if (k > listLength) {
-    throw new Error(`k is larger than the length of the linked list: ${k}`);
+  // Starting with leftNode on the head,
+  // move leftNode and rightNode down the list,
+  // maintaining a distance of k between them,
+  // until rightNode hits the end of the list
+  while (rightNode.next) {
+    leftNode = leftNode.next;
+    rightNode = rightNode.next;
   }
 
-  // STEP 2: walk to the target node
-  // Calculate how far to go, from the head,
-  // to get to the kth to last node
-  const howFarToGo = listLength - k;
-
-  currentNode = head;
-  for (let i = 0; i < howFarToGo; i++) {
-    currentNode = currentNode.next;
-  }
-
-  return currentNode;
+  // Since leftNode is k nodes behind rightNode,
+  // leftNode is now the kth to last node!
+  return leftNode;
 }
-
 const a = new LinkedListNode('Angel Food');
 const b = new LinkedListNode('Bundt');
 const c = new LinkedListNode('Cheese');
